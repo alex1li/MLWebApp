dcdc76fb0c325399c315ac4cb7360da3e1495cc9
Alex Li <afl59@cornell.edu>
2019-01-14 19:56:28 -0500

    update backend to get new tweets

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from sklearn import svm
 from sklearn import datasets
 from sklearn.externals import joblib
 from classifier import *
+from tweets import *
 import numpy as np
 
 
@@ -17,24 +18,28 @@ app = Flask(__name__)
 @app.route('/api/train', methods=['POST'])
 def train():
     # get parameters from request
-    parameters = request.get_json()
+    #parameters = request.get_json()
 
-    # read iris data set
-    #iris = datasets.load_iris()
-    #X, y = iris.data, iris.target
-
-    # fit model
-    #clf = svm.SVC(C=float(parameters['C']),
-    #             probability=True,
-    #             random_state=1)
-    #clf.fit(X, y)
     adaboost, vec, selector, trainingError, validationError = training()
     # persist model
     joblib.dump(adaboost, 'model.pkl')
     joblib.dump(vec, 'vectorizer.pkl')
     joblib.dump(selector, 'selector.pkl')
+    # get tweets and predict
+    recentTweets = getTweets()
+    preds = predictTweets(recentTweets, adaboost, vec, selector)
     #return jsonify({'accuracy': round(clf.score(X, y) * 100, 2)})
-    return jsonify({'accuracy': round(100-trainingError), 'tweetOne': 'The Building of the Wall on the Southern Border will bring down the crime rate!', 'tweetTwo': 'Im in the White House, waiting. The democrats are everywhere but Washington as people await their pay.', 'tweetThree': 'Democrats are saying DACA is not worth it and dont want it in talks','tweetFour': 'We have a massive humanitarian crisis at the Southern border'})
+    return jsonify({'accuracy': round(100-trainingError),
+    'tweetOne': recentTweets[0] + preds[0],
+    'tweetTwo': recentTweets[1] + preds[1],
+    'tweetThree': recentTweets[2] + preds[2],
+    'tweetFour': recentTweets[3] + preds[3],
+    'tweetFive': recentTweets[4] + preds[4],
+    'tweetSix': recentTweets[5] + preds[5],
+    'tweetSeven': recentTweets[6] + preds[6],
+    'tweetEight': recentTweets[7] + preds[7],
+    'tweetNine': recentTweets[8] + preds[8],
+    'tweetTen': recentTweets[9] + preds[9]})
 
 
 @app.route('/api/predict', methods=['POST'])
@@ -46,11 +51,11 @@ def predict():
     adaboost = joblib.load('model.pkl')
     vec = joblib.load('vectorizer.pkl')
     selector = joblib.load('selector.pkl')
-    
+
     tweet = vec.transform([X])
     tweet = selector.transform(tweet)
     prob = adaboost.predict_proba(tweet)
-    
+
     return jsonify([{'name': 'Trump', 'value': int(prob[0][1]*100)},
                     {'name': 'Staff', 'value': int(prob[0][0]*100)}])
 
---
+++ backend/tweets.py
@@ -0,0 +1,44 @@
+"""
+Pulls most recent tweets from Donald Trump's twitter account
+and uses a Classifier to predict if they are Trump or Staff
+
+Alexander Li (afl59), Rohan Patel (rp442)
+1/10/19
+"""
+import twitter
+
+def getTweets():
+    api = twitter.Api(consumer_key='mxSTcGP1nh6jmDG7MCB0BuOex',
+      consumer_secret='6KO0vUmpeG04xzyNz5DSOXPvRKjPfAMcblGMhP6pyDrwQdPmlM',
+      access_token_key='491599498-QrCGgzC8scH15NbDq3qIbmNCnu9meo8Rc0u2L7qA',
+      access_token_secret='iR5tDCtehji5iWnEL7wrLsFItEepwS8bUHOmpKGjlcoUm')
+
+    t = api.GetUserTimeline(screen_name="realDonaldTrump", count=10)
+    tweets = [i.AsDict() for i in t]
+    test = []
+    for t in tweets:
+        #print(t['id'], t['text'])
+        test.append(t['text'])
+    return test
+
+def predictTweets(tweets, model, vec, selector):
+    tweets = vec.transform(tweets).toarray()
+    tweets = selector.transform(tweets).astype('float32')
+    preds = model.predict(tweets).tolist()
+    for i in range(len(preds)):
+        if preds[i] > 0:
+            preds[i] = " - Donald J. Trump"
+        else:
+            preds[i] = " - White House Staff"
+    return preds
+
+#print(test)
+# print('training')
+# models, vec, selector = training()
+# print('testing')
+# test = vec.transform(test).toarray()
+# test = selector.transform(test).astype('float32')
+# for m in range(len(models)):
+#      pred = models[m].predict(test)
+#      print('model: ' + str(m))
+#      print(pred)
