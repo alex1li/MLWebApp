b5fe42d077e40a6a66ff7d3af87bf41c3f067f38
Alex Li <afl59@cornell.edu>
2019-01-20 14:57:39 -0500

    image URLs and classification

--- backend/app.py
+++ backend/app.py
@@ -31,31 +31,50 @@ def train():
     selector = joblib.load('selector.pkl')
     # get tweets and predict
     recentTweets = getTweets()
-    preds, predsNumber = predictTweets(recentTweets, adaboost, vec, selector)
+    preds, predsNumber, predsUrl = predictTweets(recentTweets, adaboost, vec, selector)
     #return jsonify({'accuracy': round(clf.score(X, y) * 100, 2)})
     #1 is Trump
     #-1 is Staff
     return jsonify({'accuracy': 'not used',
-    'tweetOne': recentTweets[0] + preds[0],
-    'tweetOnePred': predsNumber[0],
-    'tweetTwo': recentTweets[1] + preds[1],
-    'tweetTwoPred': predsNumber[1],
-    'tweetThree': recentTweets[2] + preds[2],
-    'tweetThreePred': predsNumber[2],
-    'tweetFour': recentTweets[3] + preds[3],
-    'tweetFourPred': predsNumber[3],
-    'tweetFive': recentTweets[4] + preds[4],
-    'tweetFivePred': predsNumber[4],
-    'tweetSix': recentTweets[5] + preds[5],
-    'tweetSixPred': predsNumber[5],
-    'tweetSeven': recentTweets[6] + preds[6],
-    'tweetSevenPred': predsNumber[6],
-    'tweetEight': recentTweets[7] + preds[7],
-    'tweetEightPred': predsNumber[8],
-    'tweetNine': recentTweets[8] + preds[8],
-    'tweetNinePred': predsNumber[8],
-    'tweetTen': recentTweets[9] + preds[9],
-    'tweetTenPred': predsNumber[9]})
+    'tweetOne': recentTweets[0],
+    'tweetOnePred': preds[0],
+    'tweetOneUrl': predsUrl[0],
+
+    'tweetTwo': recentTweets[1],
+    'tweetTwoPred': preds[1],
+    'tweetTwoUrl': predsUrl[1],
+
+    'tweetThree': recentTweets[2],
+    'tweetThreePred': preds[2],
+    'tweetThreeUrl': predsUrl[2],
+
+    'tweetFour': recentTweets[3],
+    'tweetFourPred': preds[3],
+    'tweetFourUrl': predsUrl[3],
+
+    'tweetFive': recentTweets[4],
+    'tweetFivePred': preds[4],
+    'tweetFiveUrl': predsUrl[4],
+
+    'tweetSix': recentTweets[5],
+    'tweetSixPred': preds[5],
+    'tweetSixUrl': predsUrl[5],
+
+    'tweetSeven': recentTweets[6],
+    'tweetSevenPred': preds[6],
+    'tweetSevenUrl': predsUrl[6],
+
+    'tweetEight': recentTweets[7],
+    'tweetEightPred': preds[7],
+    'tweetEightUrl': predsUrl[7],
+
+    'tweetNine': recentTweets[8],
+    'tweetNinePred': preds[8],
+    'tweetNineUrl': predsUrl[8],
+
+    'tweetTen': recentTweets[9],
+    'tweetTenPred': preds[9],
+    'tweetTenUrl': predsUrl[9]})
 
 
 @app.route('/api/predict', methods=['POST'])
